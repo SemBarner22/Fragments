fab8e998924fa6b430a0c5a17879cb46d20b2769
sembarner22 <ildar1001@mail.ru>
2020-10-19 17:57:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private var currentNavController: LiveData<NavController>? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupBottomNavigation()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
+    }
+
+    private fun setupBottomNavigation() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navGraphIds = listOf(
+                R.navigation.home_navigation,
+                R.navigation.dict_navigation,
+                R.navigation.chat_navigation
+        )
+        val controller = bottomNavigationView.setupWithNavController(
+                navGraphIds = navGraphIds,
+                fragmentManager = supportFragmentManager,
+                containerId = R.id.my_nav_host_fragment,
+                intent = intent
+        )
+        currentNavController = controller
+    }
+}
---
+++ app/src/main/java/com/example/fragments/fragments/NewFragment.kt
@@ -0,0 +1,24 @@
+package com.example.fragments.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+import kotlinx.android.synthetic.main.fragment_new.*
+
+class NewFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_new, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        number.text = NewFragmentArgs.fromBundle(requireArguments()).count.toString()
+    }
+}
